78962fc94774e9408e824094c5c3bcd4cffc2e13
David Avery <eismausen@gmail.com>
2021-09-23 11:57:11 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1/n2; 
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
